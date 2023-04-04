86e7700e0679b67cb7aa4336c10c2f6b7f083d82
amberwagoner <amberwagoner@outlook.com>
2023-04-03 19:42:30 -0700

    just trying stuff

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script creates a route that returns status in JSON format."""
+""" Task 7 """
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
